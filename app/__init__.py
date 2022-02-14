8ba6a29a0b4b1a15f0958cdc96fadd024170f04b
Marcos8060 <marcosgav80@gmail.com>
2022-02-14 15:17:01 +0300

    created the heroku db instance

--- app/__init__.py
+++ app/__init__.py
@@ -6,23 +6,18 @@ from  flask_migrate import Migrate, MigrateCommand
 import os
 
 
-
 app = Flask(__name__)
 
 
-
-
 # app.config['SQLALCHEMY_DATABASE_URI']='postgresql://marcos:getaways@localhost/pitch_lab'
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+# SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL').replace("postgres://", "postgresql://", 1)
+
 
 app.config['SECRET_KEY'] = '944d51c0258f07f940b031b2'
 login_manager = LoginManager(app)
 
-
 db = SQLAlchemy(app)
-@app.before_first_request
-def create_tables():
-    db.create_all()
 migrate = Migrate(app,db)
 bcrypt = Bcrypt(app)
 from app import routes
--- run.py
+++ run.py
@@ -1,7 +1,23 @@
 from app import app
+from  flask_migrate import Migrate, MigrateCommand
+from app import app,db
+from app.models import User,Pitch
+from flask_script import Manager,Server
 
 
+manager = Manager(app)
+manager.add_command('server',Server)
 
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db, User=User, Pitch = Pitch)
+
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+
+manager.add_command('server',Server)
+if __name__ == '__main__':
+    manager.run()
 
 if __name__== '__main__':
     app.run(debug=True)
